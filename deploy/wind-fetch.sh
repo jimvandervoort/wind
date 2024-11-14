ca0081946a701c0ac4e3239f2566223419915019
Jim van der Voort <jim@jim.computer>
2024-11-15 00:12:21 +0200

    fetch logs

--- deploy/wind-fetch.sh
+++ deploy/wind-fetch.sh
@@ -2,14 +2,22 @@
 
 set -Eeuxo pipefail
 
-/usr/bin/docker run \
-  --init \
-  --rm \
-  --name wind_puppet \
-  --cap-add SYS_ADMIN \
-  -v "/root/wind/dist:/wind/dist" \
-  wind \
-  node /wind/puppet.mjs
+fetch() {
+  echo "START $(date)"
 
-curl -fSs 'https://mac-wind.appspot.com/data/15min.json?offset=' > /tmp/wind.json
-mv /tmp/wind.json /root/wind/dist/
+  /usr/bin/docker run \
+    --init \
+    --rm \
+    --name wind_puppet \
+    --cap-add SYS_ADMIN \
+    -v "/root/wind/dist:/wind/dist" \
+    wind \
+    node /wind/puppet.mjs
+
+  curl -fSs 'https://mac-wind.appspot.com/data/15min.json?offset=' > /tmp/wind.json
+  mv /tmp/wind.json /root/wind/dist/
+
+  echo "END $(date)"
+}
+
+fetch 2>&1 | tee -a /root/wind/fetch.log
