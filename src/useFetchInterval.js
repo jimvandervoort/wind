4c60c52fa037d0fd9be5f5005a3b627b9a02e165
Jim van der Voort <jim@jim.computer>
2024-11-20 15:40:23 +0200

    cleanup for fun

--- src/useFetchInterval.js
+++ src/useFetchInterval.js
@@ -4,28 +4,29 @@ import {makeReport} from "./report.js";
 const interval = 10000;
 const myVersion = import.meta.env.VITE_WIND_VERSION || 'local';
 
+async function fetchAll(files) {
+  const results = await Promise.all(files.map(f => fetch(f)));
+
+  for (const r of results) {
+    if (!r.ok) {
+      throw new Error('Network response was not ok');
+    }
+  }
+
+  return Promise.all(results.map(r => r.json()));
+}
+
 export function useFetchInterval() {
   const report = ref(null);
   const error = ref(null);
 
   const fetchData = async () => {
     try {
-      // Fetch both files
-      const [dataResponse, windResponse, langeWindResponse] = await Promise.all([
-        fetch('/data.json'),
-        fetch('/wind.json'),
-        fetch('/langewind.json'),
-      ]);
-
-      // Check for errors in the response
-      if (!dataResponse.ok || !windResponse.ok || !langeWindResponse.ok) {
-        throw new Error('Network response was not ok');
-      }
-
-      // Parse the JSON data
-      const data = await dataResponse.json();
-      const macWind = await windResponse.json();
-      const langeWind = await langeWindResponse.json();
+      const [data, macWind, langeWind] = await fetchAll([
+        '/data.json',
+        '/wind.json',
+        '/langewind.json',
+      ])
 
       const theirVersion = data[0].version;
       console.log({theirVersion, myVersion});
@@ -36,7 +37,6 @@ export function useFetchInterval() {
         }, 30000);
       }
 
-      // Process the data with makeReport
       report.value = makeReport(data, macWind, langeWind);
       error.value = null;
     } catch (err) {
