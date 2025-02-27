489014dfaa9beb253f448d32cd9909f34a98e32a
Jim van der Voort <jim@jim.computer>
2025-02-27 12:38:54 +0200

    use rsync for deployments

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -9,16 +9,16 @@ ssh wind chmod 777 wind/dist
 
 npm run build
 rm -f dist/*.json
-scp -r dist/* wind:wind/dist
+rsync -av dist/* wind:wind/dist/
 
 
-scp deploy/wind-*.sh wind:/bin/
+rsync -av deploy/wind-*.sh wind:/bin/
 ssh wind 'chmod +x /bin/wind-*.sh'
-scp deploy/*.{service,timer} wind:/etc/systemd/system
+rsync -av deploy/*.{service,timer} wind:/etc/systemd/system
 
 docker --context wind build --build-arg "VITE_WIND_VERSION=$VITE_WIND_VERSION" -t wind .
 
-scp deploy/*.conf wind:wind
+rsync -av deploy/*.conf wind:wind/
 
 ssh wind systemctl daemon-reload
 ssh wind systemctl enable --now wind-nginx
