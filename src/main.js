202fde47e6fde5f66bc5cc98eebe284bf854a8f6
Jim van der Voort <jim@jim.computer>
2025-04-10 08:18:39 +0200

    Do install use api/auth plugin until used

--- src/main.js
+++ src/main.js
@@ -3,12 +3,12 @@ import './style.css'
 import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
-import authPlugin from './plugins/auth'
-import apiPlugin from './plugins/api'
+// import authPlugin from './plugins/auth'
+// import apiPlugin from './plugins/api'
 
 const app = createApp(App)
 
 app.use(router)
-app.use(authPlugin)
-app.use(apiPlugin)
+// app.use(authPlugin)
+// app.use(apiPlugin)
 app.mount('#app')
