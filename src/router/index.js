dd99e1768bd3b4680f984d0523f5d65230ad5b22
Jim van der Voort <jim@jim.computer>
2025-04-10 08:17:50 +0200

    Do not include login page for now

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
-import Login from '../views/Login.vue'
+// import Login from '../views/Login.vue'
 
 const routes = [
   {
@@ -9,11 +9,11 @@ const routes = [
     component: Home,
     props: true
   },
-  {
-    path: '/login',
-    name: 'Login',
-    component: Login
-  }
+  // {
+  //   path: '/login',
+  //   name: 'Login',
+  //   component: Login
+  // }
 ]
 
 const router = createRouter({
