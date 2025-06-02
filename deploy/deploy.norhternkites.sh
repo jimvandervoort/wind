ee504149c2bfd935d612d38ed739fba779e3d326
Jim van der Voort <jim@jim.computer>
2025-06-02 18:25:18 +0200

    Add CE (custom element) build mode

--- deploy/deploy.norhternkites.sh
+++ deploy/deploy.norhternkites.sh
@@ -4,12 +4,14 @@ set -Eeuxo pipefail
 
 export VITE_WIND_VERSION="$(date -u '+%Y%m%d_%H%M')"
 export VITE_WIND_ENDPOINT="https://jimwind.com"
-npm run build
+
+npm run build:ce
 rm -f dist/*.json
 rm -f dist/*.html
+
 rsync -av dist/* wind:wind/dist/
 
 rsync -av deploy/nginx.conf wind:wind/
 ssh wind systemctl restart wind-nginx
 
-ssh wind 'ln -sf "$(basename $(ls -t /root/wind/dist/assets/index-*.js | head -n1))" /root/wind/dist/assets/latest.js'
+ssh wind 'ln -sf "$(basename $(ls -t /root/wind/dist/assets/main-ce-*.js | head -n1))" /root/wind/dist/assets/latest.js'
--- src/components/KiteRegion.vue
+++ src/components/KiteRegion.vue
@@ -1,26 +1,19 @@
 <script setup>
-import "../style.css";
 import { ref, computed, watch } from 'vue';
 import KiteSpots from './KiteSpots.vue';
-import { mapRangeClamp } from '../range';
 import { useFetchInterval } from '../useFetchInterval';
-import { PlusIcon, PencilIcon } from '@heroicons/vue/24/outline';
+import { PencilIcon } from '@heroicons/vue/24/outline';
 import WindSpeedControls from './WindSpeedControls.vue';
 const props = defineProps({
   region: {
     type: String,
-    default: 'tarifa'
+    required: true
   },
 });
 
 const isCustomisable = computed(() => props.region === 'myspots');
 const windThreshold = ref(Number(localStorage.getItem('windThreshold')) || 10);
 const roundedWindThreshold = ref(Math.round(windThreshold.value));
-const showSettings = ref(false);
-
-const toggleSettings = () => {
-  showSettings.value = !showSettings.value;
-}
 
 watch(windThreshold, (newValue) => {
   localStorage.setItem('windThreshold', Math.round(newValue));
@@ -43,40 +36,22 @@ const { report, error } = useFetchInterval(roundedWindThreshold);
     </p>
   </div>
 
+  <div class="flex flex-col p-8 gap-4" v-if="isCustomisable">
+    <div class="flex flex-col max-w-md">
+      <router-link v-if="report && report.length === 0" to="/myspots/edit" class="flex items-center justify-center gap-2 px-4 py-2 rounded-lg fira-code bg-gradient-to-br from-emerald-500 to-teal-500 text-white">
+        <span class="font-semibold text-2xl">+</span>
+        <span>Add Spots</span>
+      </router-link>
+      <router-link v-else-if="report && report.length > 0" to="/myspots/edit" class="flex items-center justify-center gap-2 px-4 py-2 rounded-lg fira-code border-2 border-white">
+        <PencilIcon class="h-5 w-5" />
+        <span>Edit Spots</span>
+      </router-link>
+    </div>
+  </div>
+
   <div class="flex flex-col p-8 pl-[6.66rem] pr-2 gap-4">
     <p class="pt-0 fira-code" v-if="error">Failed to load latest wind data. Please make sure you're connected to the internet.</p>
     <p class="pt-0 fira-code" v-if="report && report.length > 0">For feedback, compliments and suggestions, email: <a href="mailto:wind@jim.computer" class="underline hover:text-blue-300">wind@jim.computer</a></p>
   </div>
   <WindSpeedControls v-if="report && report.length > 0" v-model="windThreshold" />
 </template>
-
-<style>
-.slider {
-  -webkit-appearance: none;
-  appearance: none;
-  width: 100%;
-  height: .4rem;
-
-  background: none;
-  border: 2px solid #ccc;
-  outline: none;
-}
-
-.slider::-webkit-slider-thumb {
-  -webkit-appearance: none;
-  appearance: none;
-  width: var(--scale);
-  height: var(--scale);
-  background: url('../assets/george.png') no-repeat center center;
-  background-size: cover;
-}
-
-.slider::-moz-range-thumb {
-  appearance: none;
-  border: none;
-  width: var(--scale);
-  height: var(--scale);
-  background: url('../assets/george.png') no-repeat center center;
-  background-size: cover;
-}
-</style>
