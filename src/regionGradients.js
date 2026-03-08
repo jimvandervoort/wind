f9b0878b5e82c02a976b4b0fdd3eac6ad10b9c57
Max Tretter <maxtretter@mailbox.org>
2026-03-08 14:15:06 +0100

    style: Make a gradient for germany

--- src/regionGradients.js
+++ src/regionGradients.js
@@ -3,6 +3,7 @@ export const regionGradients = {
   capetown: 'bg-gradient-to-br from-emerald-500 to-teal-500',
   holland: 'bg-gradient-to-br from-orange-500 to-amber-500',
   sweden: 'bg-gradient-to-br from-blue-500 to-indigo-500',
+  germany: 'bg-gradient-to-tl from-purple-400 to-indigo-500',
   myspots: 'bg-gradient-to-br from-amber-500 to-yellow-500',
 };
 
